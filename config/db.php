f8c3ba5a126489ca24a7754d1f2338aec95a8ef7
ko610 <83527519+ko610@users.noreply.github.com>
2021-05-04 08:50:32 +0900

    Create db.php

---
+++ config/db.php
@@ -0,0 +1,10 @@
+<?php
+return [
+  'class' => 'yii\db\Connection',
+  'dsn' => 'mysql:host=localhost;dbname=apple_data',
+  'username' => 'apple_user',
+  'passward' => 'kanato2710',
+  'charset' => 'utf-8',
+];
+
+?>
